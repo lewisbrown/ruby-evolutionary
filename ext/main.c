fb8a66c8a052f227516713ec67fafb4094eee0d5
Rob Watson <rfwatson@gmail.com>
2009-01-19 23:35:29 +0000

    simple 2-point crossover but so tired cannot see screen anymore

--- ext/main.c
+++ ext/main.c
@@ -4,12 +4,18 @@
 VALUE Module = Qnil;
 VALUE Class  = Qnil;
 
+struct candidate {
+    char *genome;
+    int fitness;
+};
+
 void Init_evolutionary();
 VALUE method_initialize(VALUE self, VALUE phenome);
 VALUE method_evolve(VALUE self, VALUE vN_gens, VALUE vPop_size);
 char* tournament_select(VALUE phenome, char **pop, int pop_size);
 void populate(char** pop, int g_size, int pop_size);
 void free_pop(char **pop, int pop_size);
+void two_point_crossover(char *buf, char* g1, char* g2, int g_size);
 
 void Init_evolutionary() {
 	Module = rb_define_module("Evolutionary");
@@ -47,18 +53,35 @@ char* tournament_select(VALUE phenome, char **pop, int pop_size) {
 
     a = pop[random() % pop_size];
     b = pop[random() % pop_size];
-    
-    printf("a is %s and b is %s\n", a, b);
-    
+        
     rb_funcall(phenome, rb_intern("decode"), 1, rb_str_new2(a));
     fa = FIX2INT(rb_funcall(phenome, rb_intern("fitness"), 0));
 
     rb_funcall(phenome, rb_intern("decode"), 1, rb_str_new2(b));
     fb = FIX2INT(rb_funcall(phenome, rb_intern("fitness"), 0));
-    
-    printf("%i, %i\n", fa, fb);
 
     return fa > fb ? a : b;
+}
+
+void two_point_crossover(char *buf, char *g1, char *g2, int g_size) {
+    int p1 = (random() % (g_size - 2)) + 1;
+    int p2 = p1 + (random() % (g_size - p1));
+    
+    int i;
+    for(i=0; i<p1; ++i)
+        buf[i] = g1[i];
+    for(;i<p2; ++i)
+        buf[i] = g2[i];
+    for(;i<g_size; ++i)
+        buf[i] = g1[i];
+}
+
+char* weakest(VALUE phoneme, char** pop, int pop_size) {
+    int i=0, fitness=0, low_fitness=0;
+    char *weakest;
+    while(i < pop_size) {
+        //fitness = FIX2INT(rb_funcall);
+    }
 }
 
 VALUE method_initialize(VALUE self, VALUE phenome) {
@@ -70,6 +93,8 @@ VALUE method_evolve(VALUE self, VALUE vN_gens, VALUE vPop_size) {
     int g, pop_size, g_size;
     char **pop;
 
+    char *child = (char *)malloc(sizeof(char) * g_size);
+    
     VALUE phenome = rb_ivar_get(self, rb_intern("@phenome"));
     VALUE genotype = rb_funcall(phenome, rb_intern("encode"), 0);
 
@@ -82,12 +107,14 @@ VALUE method_evolve(VALUE self, VALUE vN_gens, VALUE vPop_size) {
     for(g=0; g < FIX2INT(vN_gens); ++g) {
         if(g % 250 == 0)
             printf("Processing generation %d\n", g);
-            
-        printf("selected %s\n", tournament_select(phenome, pop, pop_size));
+        
+        char* c1 = tournament_select(phenome, pop, pop_size);
+        char* c2 = tournament_select(phenome, pop, pop_size);
+        
+        two_point_crossover(child, c1, c2, g_size);
     }
     
-
     free_pop(pop, pop_size);        
+    free(child);
     return Qnil;
 }
-
